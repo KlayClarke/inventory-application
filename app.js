dc25b30c566f716312a162eba957c5e63d13663c
Klay Clarke <klayaclarke@gmail.com>
2022-03-17 19:17:54 -0400

    fix mongoose uri parse issue

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 // set up mongoose connection
 var mongoose = require("mongoose");
 var mongoDB = process.env.MONGODB_URI;
-mongoose.connect(mongoDB, { useNewURLParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
