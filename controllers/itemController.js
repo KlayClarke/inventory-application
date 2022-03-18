627b1eee28b64a52be47f95b70a76e95436a4664
Klay Clarke <clarkeklay@gmail.com>
2022-03-18 18:29:39 -0400

    add code for item create get and post routes - add code for item delete get and post routes

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -72,22 +72,154 @@ exports.item_detail = function (req, res, next) {
 
 // display item create form
 exports.item_create_get = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: ITEM CREATE GET");
+  async.parallel(
+    {
+      categories: function (callback) {
+        Category.find(callback);
+      },
+    },
+    function (err, results) {
+      if (err) return next(err);
+      res.render("item_form", {
+        title: "Create Item",
+        categories: results.categories,
+      });
+    }
+  );
 };
 
 // handle item create
-exports.item_create_post = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: ITEM CREATE POST");
-};
+exports.item_create_post = [
+  // convert category to array
+  (req, res, next) => {
+    if (!req.body.category instanceof Array) {
+      if (typeof req.body.category === "undefined") req.body.category = [];
+      else req.body.category = new Array(req.body.category);
+    }
+    next();
+  },
+  // validate and sanitize
+  body("name", "Name must be specified").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("category.*").escape(), // use wildcard * tag to individually validate each of the genre array entries
+  body("price", "Price must be specified")
+    .trim()
+    .isLength({ min: 0 })
+    .toFloat()
+    .escape(),
+
+  // process request after validation and sanitization
+  (req, res, next) => {
+    // extract the validation errors from request
+    const errors = validationResult(req);
+    // create item object with escaped and trimmed data
+    let item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+    });
+
+    if (!errors.isEmpty()) {
+      // there are errors
+      // get all categories
+      async.parallel(
+        {
+          categories: function (callback) {
+            Category.find(callback);
+          },
+        },
+        function (err, results) {
+          // mark user selected categories as checked
+          for (let i = 0; i < results.categories.length; i++) {
+            if (item.category.indexOf(results.categories[i]._id) > -1) {
+              // set category to checked
+              results.categories[i].checked = "true";
+            }
+          }
+          res.render("item_form", {
+            title: "Create Item",
+            item: item,
+            categories: results.categories,
+            errors: errors.array(),
+          });
+          return;
+        }
+      );
+    } else {
+      // data from form is valid
+      item.save(function (err) {
+        if (err) return next(err);
+        // successful - redirect to new item record
+        res.redirect(item.url);
+      });
+    }
+  },
+];
 
 // display item delete form
 exports.item_delete_get = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: ITEM DELETE GET");
+  async.parallel(
+    {
+      // find item and item instances
+      item: function (callback) {
+        Item.findById(req.params.id).exec(callback);
+      },
+      iteminstances: function (callback) {
+        ItemInstance.find({ item: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) return next(err);
+      if (results.item == null) {
+        // item not found
+        res.redirect("/inventory/items");
+      }
+      // success, render confirmation page
+      res.render("item_delete", {
+        title: "Delete Item",
+        item: results.item,
+        iteminstances: results.iteminstances,
+      });
+    }
+  );
 };
 
 // handle item delete
 exports.item_delete_post = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: ITEM DELETE POST");
+  async.parallel(
+    {
+      item: function (callback) {
+        Item.findById(req.body.itemid).exec(callback);
+      },
+      iteminstances: function (callback) {
+        ItemInstance.find({ item: req.params.itemid }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) return next(err);
+      // success
+      if (results.iteminstances.length > 0) {
+        // item has instances - render form again
+        res.render("item_delete", {
+          title: "Delete Item",
+          item: results.item,
+          iteminstances: results.iteminstances,
+        });
+        return;
+      } else {
+        // item has no instances - free to delete object
+        Item.findByIdAndDelete(req.body.itemid, function deleteItem(err) {
+          if (err) return next(err);
+          // success - redirect to items list
+          res.redirect("/inventory/items");
+        });
+      }
+    }
+  );
 };
 
 // display item update form
